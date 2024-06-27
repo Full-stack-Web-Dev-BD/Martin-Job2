f80e61b9b4fa9cc3261219a7bb14c33107a453d7
Dappmaster (FullstackBlockchain Developer) <60449971+Full-stack-Web-Dev-BD@users.noreply.github.com>
2024-06-27 13:59:23 +0600

    rm uns

--- in.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-# Connect to MongoDB
-client = MongoClient('mongodb+srv://alamin:1zqbsg2vBlyY1bce@cluster0.sngd13i.mongodb.net/mvp2?retryWrites=true&w=majority')
-db = client['mvp2']
-sp_upc_lookup = db['sp_upc_lookup']
-uk_daily_data = db['UK_Daily_Data']
-
